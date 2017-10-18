95000c7dd964f8841b24a233a9ac299c0adfda60
Vincent Driessen <me@nvie.com>
2017-10-18 16:37:39 +0200

    Add tests and try out how dispatching works with these types

--- src/Result.js
+++ src/Result.js
@@ -1,30 +1,93 @@
 // @flow
 
+/**
+ * Result error value
+ *     = Ok value
+ *     | Err error
+ */
+
+const Ok = 'Ok';
+const Err = 'Err';
+
 // prettier-ignore
-opaque type _ResultT<E, D> =
-    | { _ResultT: 'Err', error: E }
-    | { _ResultT: 'Ok', value: D };
+opaque type _ResultT<E, T> =
+    | { type: typeof Err, error: E }
+    | { type: typeof Ok, value: T };
 
-export default class Result<E, D> {
-    _r: _ResultT<E, D>;
+/**
+ * Represents a union type that's either a legit value or an error:
+ *
+ *     Result error value
+ *         = Ok value
+ *         | Err error
+ *
+ */
+export default class Result<E, T> {
+    _r: _ResultT<E, T>;
 
-    constructor(r: _ResultT<E, D>) {
+    /**
+     * **Do not call this constructor directly!**  Use either `Result.ok()` or
+     * `Result.err()` to construct a new Result instance.
+     */
+    constructor(r: _ResultT<E, T>) {
         this._r = r;
     }
 
-    static ok(value: D): Result<E, D> {
-        return new Result({ _ResultT: 'Ok', value });
+    /**
+     * Create a new Result instance representing a successful computation.
+     */
+    static ok(value: T): Result<E, T> {
+        return new Result({ type: Ok, value });
     }
 
-    static err(error: E): Result<E, D> {
-        return new Result({ _ResultT: 'Err', error });
+    /**
+     * Create a new Result instance representing a failed computation.
+     */
+    static err(error: E): Result<E, T> {
+        return new Result({ type: Err, error });
     }
 
     isOk(): boolean {
-        return this._r._ResultT === 'Ok';
+        return this._r.type === Ok;
     }
 
     isErr(): boolean {
-        return this._r._ResultT === 'Err';
+        return this._r.type === Err;
+    }
+
+    withDefault(defaultValue: T): T {
+        const r = this._r;
+        if (r.type === Ok) {
+            return r.value;
+        } else {
+            return defaultValue;
+        }
+    }
+
+    toMaybe(): void | T {
+        const r = this._r;
+        if (r.type === Ok) {
+            return r.value;
+        } else {
+            return undefined;
+        }
+    }
+
+    /**
+     * Unwrap the value from this Result instance if this is an "Ok" result.
+     * Otherwise, will throw the "Err" error via a runtime exception.
+     */
+    unwrap(): T {
+        const r = this._r;
+        if (r.type === Ok) {
+            return r.value;
+        } else {
+            throw r.error;
+        }
+    }
+
+    dispatch<O>(errCallback: E => O, okCallback: T => O): O {
+        const r = this._r;
+        return r.type === Ok ? okCallback(r.value) : errCallback(r.error);
     }
 }
--- src/__tests__/Result.test.js
+++ src/__tests__/Result.test.js
@@ -3,17 +3,33 @@
 import Result from '../Result';
 
 describe('Result', () => {
-    it('result creation', () => {
-        const r1 = Result.ok(42);
+    const r1 = Result.ok(42);
+    const r2 = Result.ok("I'm a string");
+    const r3 = Result.err(new Error('Proper JS error'));
+    const r4 = Result.err('Error as a simple string value');
+
+    it('result inspection', () => {
         expect(r1.isOk()).toBe(true);
         expect(r1.isErr()).toBe(false);
-
-        const r2 = Result.ok('ohai');
         expect(r2.isOk()).toBe(true);
         expect(r2.isErr()).toBe(false);
-
-        const r3 = Result.err('oops');
         expect(r3.isOk()).toBe(false);
         expect(r3.isErr()).toBe(true);
+        expect(r4.isOk()).toBe(false);
+        expect(r4.isErr()).toBe(true);
+    });
+
+    it('result dispatching', () => {
+        const [v1, v2, v3, v4] = [r1, r2, r3, r4].map(r =>
+            // prettier-ignore
+            r.dispatch(
+                () => "I'm an error",
+                () => "I'm a success",
+            )
+        );
+        expect(v1).toBe("I'm a success");
+        expect(v2).toBe("I'm a success");
+        expect(v3).toBe("I'm an error");
+        expect(v4).toBe("I'm an error");
     });
 });
