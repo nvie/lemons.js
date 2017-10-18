a7ff777255f0bee71817ff6a407ff456cc29ffe0
Vincent Driessen <me@nvie.com>
2017-10-18 22:09:09 +0200

    Add LazyResult class

---
+++ src/LazyResult.js
@@ -0,0 +1,135 @@
+// @flow
+
+/**
+ * LazyResult error result
+ *     = Initial
+ *     | Loading
+ *     | Failure error
+ *     | Success result
+ */
+
+const Initial = 'Initial';
+const Loading = 'Loading';
+const Failure = 'Failure';
+const Success = 'Success';
+
+// prettier-ignore
+opaque type $LazyResultT<E, T> =
+    | { type: typeof Initial }
+    | { type: typeof Loading }
+    | { type: typeof Failure, error: E }
+    | { type: typeof Success, result: T };
+
+/**
+ * Represents a union type that's in either one of these four stages: Initial,
+ * Loading, Failure, or Success.
+ *
+ *     LazyResult error result
+ *         = Initial
+ *         | Loading
+ *         | Failure error
+ *         | Success result
+ *
+ */
+export default class LazyResult<E, T> {
+    _r: $LazyResultT<E, T>;
+
+    /**
+     * **Do not call this constructor directly!**  Use either `Maybe.just()` or
+     * `Maybe.nothing()` to construct a new Maybe instance.
+     */
+    constructor(r: $LazyResultT<E, T>) {
+        this._r = r;
+    }
+
+    /**
+     * Create a new LazyResult instance representing a computation that has not
+     * yet begun.
+     */
+    static initial(): LazyResult<E, T> {
+        return new LazyResult({ type: Initial });
+    }
+
+    /**
+     * Create a new LazyResult instance representing a computation that has
+     * started, but not yet finished.
+     */
+    static loading(): LazyResult<E, T> {
+        return new LazyResult({ type: Loading });
+    }
+
+    /**
+     * Create a new LazyResult instance representing a failure.
+     */
+    static failure(error: E): LazyResult<E, T> {
+        return new LazyResult({ type: Failure, error });
+    }
+
+    /**
+     * Create a new LazyResult instance representing a successfully computed
+     * value.
+     */
+    static success(result: T): LazyResult<E, T> {
+        return new LazyResult({ type: Success, result });
+    }
+
+    toString() {
+        const r = this._r;
+        if (r.type === Initial) {
+            return 'Initial';
+        } else if (r.type === Loading) {
+            return 'Loading';
+        } else if (r.type === Failure) {
+            return `Failure(${(r.error: any)})`;
+        } else {
+            return `Success(${(r.result: any)})`;
+        }
+    }
+
+    isInitial(): boolean {
+        return this._r.type === Initial;
+    }
+
+    isLoading(): boolean {
+        return this._r.type === Loading;
+    }
+
+    isFailure(): boolean {
+        return this._r.type === Failure;
+    }
+
+    isSuccess(): boolean {
+        return this._r.type === Success;
+    }
+
+    dispatch<O>(
+        initialCallback: () => O,
+        loadingCallback: () => O,
+        failureCallback: E => O,
+        successCallback: T => O
+    ): O {
+        const r = this._r;
+        if (r.type === Initial) {
+            return initialCallback();
+        } else if (r.type === Loading) {
+            return loadingCallback();
+        } else if (r.type === Failure) {
+            return failureCallback(r.error);
+        } else {
+            return successCallback(r.result);
+        }
+    }
+}
+
+const _Initial = <E, T>(): LazyResult<E, T> => LazyResult.initial();
+const _Loading = <E, T>(): LazyResult<E, T> => LazyResult.loading();
+const _Failure = <E, T>(error: E): LazyResult<E, T> => LazyResult.failure(error);
+const _Success = <E, T>(result: T): LazyResult<E, T> => LazyResult.success(result);
+
+// prettier-ignore
+export {
+    _Initial as Initial,
+    _Loading as Loading,
+    _Failure as Failure,
+    _Success as Success,
+};
---
+++ src/__tests__/LazyResult.js
@@ -0,0 +1,62 @@
+// @flow
+
+import LazyResult, { Initial, Loading, Failure, Success } from '../LazyResult';
+
+describe('LazyResult', () => {
+    const r1 = LazyResult.initial();
+    const r2 = LazyResult.loading();
+    const r3 = LazyResult.failure('oops');
+    const r4 = LazyResult.success('awesome');
+
+    it('toString', () => {
+        expect(r1.toString()).toBe('Initial');
+        expect(r2.toString()).toBe('Loading');
+        expect(r3.toString()).toBe('Failure(oops)');
+        expect(r4.toString()).toBe('Success(awesome)');
+    });
+
+    it('inspection', () => {
+        expect(r1.isInitial()).toBe(true);
+        expect(r1.isLoading()).toBe(false);
+        expect(r1.isFailure()).toBe(false);
+        expect(r1.isSuccess()).toBe(false);
+
+        expect(r2.isInitial()).toBe(false);
+        expect(r2.isLoading()).toBe(true);
+        expect(r2.isFailure()).toBe(false);
+        expect(r2.isSuccess()).toBe(false);
+
+        expect(r3.isInitial()).toBe(false);
+        expect(r3.isLoading()).toBe(false);
+        expect(r3.isFailure()).toBe(true);
+        expect(r3.isSuccess()).toBe(false);
+
+        expect(r4.isInitial()).toBe(false);
+        expect(r4.isLoading()).toBe(false);
+        expect(r4.isFailure()).toBe(false);
+        expect(r4.isSuccess()).toBe(true);
+    });
+
+    it('convenience constructors', () => {
+        expect(Initial().isInitial()).toBe(true);
+        expect(Loading().isLoading()).toBe(true);
+        expect(Failure().isFailure()).toBe(true);
+        expect(Success().isSuccess()).toBe(true);
+    });
+
+    it('dispatching', () => {
+        const [v1, v2, v3, v4] = [r1, r2, r3, r4].map(m =>
+            // prettier-ignore
+            m.dispatch(
+                () => '',
+                () => 'Loading...',
+                e => `Error: ${e}`,
+                data => `Data: ${data}`,
+            )
+        );
+        expect(v1).toBe('');
+        expect(v2).toBe('Loading...');
+        expect(v3).toBe('Error: oops');
+        expect(v4).toBe('Data: awesome');
+    });
+});
