4cf3a5b1a29468c36f3d5123be8490e5f12effc0
Vincent Driessen <me@nvie.com>
2018-09-23 19:23:14 +0200

    Add typescript support

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+import LazyResult, { Failure, Initial, Loading, Success } from './LazyResult';
+import Maybe, { Just, Nothing } from './Maybe';
+import Result, { Err, Ok } from './Result';
+
+export { Err, Failure, Initial, Just, LazyResult, Loading, Maybe, Nothing, Ok, Result, Success };
